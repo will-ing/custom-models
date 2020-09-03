1d097df73acdd054608e38df4aecd5775cf201ff
will-ing <will-ing@github.com>
2020-09-02 19:29:04 -0700

    finished lab

--- users/models.py
+++ users/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
-    # add additional fields in here
+    email = models.EmailField(('email address'), blank=True, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.username
